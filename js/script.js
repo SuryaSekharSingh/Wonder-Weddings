205aa976ceeff7a0ffd1ed237d942d4476c4195b
original taste <originaltaste05@gmail.com>
2024-01-08 13:26:04 +0530

    fixed the script.js src link and removed some dead reference to document

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-let menu = document.querySelector('#menu-button');
-let navbar = document.querySelector('.header .navbar');
+// let menu = document.querySelector('#menu-button');
+// let navbar = document.querySelector('.header .navbar');
 
-menu.onclick = ()=> {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+// menu.onclick = ()=> {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
 
-window.onscroll = ()=> {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+// window.onscroll = ()=> {
+    // menu.classList.remove('fa-times');
+    // navbar.classList.remove('active');
+// }
 
 var swiper = new Swiper(".home-slider", {
     loop:true,
