64390c4465006a535df551d263b7d1fc3ded5f09
original taste <originaltaste05@gmail.com>
2024-01-09 00:57:47 +0530

    disabled booked dates

--- include/get_booked_dates.php
+++ include/get_booked_dates.php
@@ -1,6 +1,10 @@
 <?php 
-include("include/db_connect.php");
-$sql = "select wedding_date from booking";
+include("./db_connect.php");
+
+$month = $_GET['month'];
+$year = $_GET['year'];
+
+$sql = "select wedding_date from booking where MONTH(wedding_date)=$month and YEAR(wedding_date)=$year";
 $result = mysqli_query($conn,$sql);
 if($result){
     $data = array();
